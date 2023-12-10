6fc66e10d749f66d0dff5c40b3ee27df1c90cdd5
emirhanaricii <emirhan190341@gmail.com>
2023-12-10 18:04:46 +0300

    Revise AccountControllerTest

--- src/test/java/com/emirhanarici/bankapplication/controller/AccountControllerTest.java
+++ src/test/java/com/emirhanarici/bankapplication/controller/AccountControllerTest.java
@@ -3,9 +3,13 @@ package com.emirhanarici.bankapplication.controller;
 import com.emirhanarici.bankapplication.base.BaseControllerTest;
 import com.emirhanarici.bankapplication.dto.AccountDTO;
 import com.emirhanarici.bankapplication.mapper.AccountMapper;
+import com.emirhanarici.bankapplication.payload.request.CreateCreditRequest;
+import com.emirhanarici.bankapplication.payload.request.CreatePhoneBillPaymentRequest;
+import com.emirhanarici.bankapplication.payload.request.CreateWithdrawalRequest;
 import com.emirhanarici.bankapplication.payload.request.CreatedAccountRequest;
 import com.emirhanarici.bankapplication.payload.response.AccountDetailInfo;
 import com.emirhanarici.bankapplication.payload.response.CreatedAccountResponse;
+import com.emirhanarici.bankapplication.payload.response.TransactionResponse;
 import com.emirhanarici.bankapplication.service.AccountService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -60,11 +64,11 @@ public class AccountControllerTest extends BaseControllerTest {
                 .transactionDTOs(accountDTO.getTransactionDTOs())
                 .build();
 
-        // when
+        // When
         when(accountService.create(request)).thenReturn(accountDTO);
         when(accountMapper.toAccountResponse(accountDTO)).thenReturn(createdAccountResponse);
 
-        // then
+        // Then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
@@ -99,11 +103,11 @@ public class AccountControllerTest extends BaseControllerTest {
                 .transactionDTOs(accountDTO.getTransactionDTOs())
                 .build();
 
-        // when
+        // When
         when(accountService.getAccountByAccountNumber(accountNumber)).thenReturn(accountDTO);
         when(accountMapper.toAccountDetailInfo(accountDTO)).thenReturn(accountDetailInfo);
 
-        // then
+        // Then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/account/account-number/{accountNumber}", accountNumber))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
@@ -111,6 +115,94 @@ public class AccountControllerTest extends BaseControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").isString())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.balance").isNumber())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.transactionDTOs").isArray());
+
+    }
+
+    @Test
+    public void givenCreateCreditRequest_whenCredit_ReturnTransactionResponse() throws Exception {
+
+        // Given
+        CreateCreditRequest createCreditRequest = CreateCreditRequest.builder()
+                .accountNumber("123456789")
+                .amount(100.0)
+                .build();
+
+        TransactionResponse transactionResponse = TransactionResponse.builder()
+                .status("OK")
+                .approvalCode("approvalCode")
+                .build();
+
+        // When
+        when(accountService.credit(createCreditRequest)).thenReturn(transactionResponse);
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/credit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createCreditRequest)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvalCode").value("approvalCode"));
+
+
+    }
+
+    @Test
+    public void givenCreateWithdrawalRequest_whenDebit_ReturnTransactionResponse() throws Exception {
+
+        // Given
+        CreateWithdrawalRequest createWithdrawalRequest = CreateWithdrawalRequest.builder()
+                .accountNumber("123456789")
+                .amount(100.0)
+                .build();
+
+        TransactionResponse transactionResponse = TransactionResponse.builder()
+                .status("OK")
+                .approvalCode("approvalCode")
+                .build();
+
+        // When
+        when(accountService.debit(createWithdrawalRequest)).thenReturn(transactionResponse);
+
+        // Then
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/debit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createWithdrawalRequest)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvalCode").value("approvalCode"));
+
+
+    }
+
+    @Test
+    public void givenCreatePhoneBillPaymentRequest_whenPayment_ReturnTransactionResponse() throws Exception {
+
+        // Given
+        CreatePhoneBillPaymentRequest createPhoneBillPaymentRequest = CreatePhoneBillPaymentRequest.builder()
+                .accountNumber("123456789")
+                .amount(50.0)
+                .build();
+
+        TransactionResponse transactionResponse = TransactionResponse.builder()
+                .status("OK")
+                .approvalCode("approvalCode")
+                .build();
+
+        // When
+        when(accountService.payment(createPhoneBillPaymentRequest)).thenReturn(transactionResponse);
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/payment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createPhoneBillPaymentRequest)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("OK"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.approvalCode").value("approvalCode"));
+
 
     }
 
