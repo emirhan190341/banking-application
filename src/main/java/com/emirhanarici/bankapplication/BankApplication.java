b4b8fb0bbe2d2289949e233e5e9430fb7adc074d
emirhanaricii <emirhan190341@gmail.com>
2023-12-13 17:19:08 +0300

    Prometheus Configuration

--- src/main/java/com/emirhanarici/bankapplication/BankApplication.java
+++ src/main/java/com/emirhanarici/bankapplication/BankApplication.java
@@ -1,10 +1,8 @@
 package com.emirhanarici.bankapplication;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankApplication {
